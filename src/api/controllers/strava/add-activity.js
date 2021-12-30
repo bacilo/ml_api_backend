f087114b0a2057325a6a57be017afb8d434adf71
bacilo <i@pedropaf.com>
2021-12-30 21:43:01 +0100

    temporary functionality, still not 100%

--- src/api/controllers/strava/add-activity.js
+++ src/api/controllers/strava/add-activity.js
@@ -2,7 +2,8 @@ import { Activity } from '../../../models/index.js';
 import { errorHelper, logger, getText } from '../../../utils/index.js';
 
 export default async (req, res) => {
-  console.log(req.query)
+  console.log('new activity')
+  console.log(req)
   if(req.query.aspect_type == "create" && req.query.object_type == "activity"){
     return res.status(200).json({
       'new activity': req.query.object_id
